74718f98ec5e016f5324b525ccbd1edc8e52c8f1
Fred Sharples <fredsharples@gmail.com>
2016-05-21 16:35:42 -0700

    added counter and win UI text

--- Assets/_scenes/Scripts/PlayerController.cs
+++ Assets/_scenes/Scripts/PlayerController.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
 	public float speed;
+
+	public Text countText;
+	public Text winText;
 
 	private Rigidbody rb;
 	private int count;
@@ -12,6 +16,8 @@ public class PlayerController : MonoBehaviour {
 	{
 		rb = GetComponent<Rigidbody>();
 		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 
 	void FixedUpdate ()
@@ -27,6 +33,13 @@ public class PlayerController : MonoBehaviour {
 		if (other.gameObject.CompareTag ("Pick Up")) {
 			other.gameObject.SetActive (false);
 			count = count + 1;
+			SetCountText ();
 		}
+		if (count >= 12){
+			winText.text = "You WIN!";
+		}
+	}
+	void SetCountText (){
+		countText.text = "Count: " + count.ToString ();
 	}
 }
